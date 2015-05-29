84c09387a4db936ee447622f5fb7000069454e1e
cbuckey <caroline@udacity.com>
2015-05-29 16:04:45 -0700

    bunch of updates to 'improve_street_names'

--- python/improve_street_names.py
+++ python/improve_street_names.py
@@ -1,7 +1,7 @@
 """
 Your task in this exercise has two steps:
 
-- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix 
+- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix
     the unexpected street types to the appropriate ones in the expected list.
     You have to add mappings only for the actual problems you find in this OSMFILE,
     not a generalized solution, since that may and will depend on the particular area you are auditing.
@@ -10,16 +10,32 @@ Your task in this exercise has two steps:
     We have provided a simple test so that you see what exactly is expected
 """
 import xml.etree.cElementTree as ET
-from collections import defaultdict
 import re
 import pprint
 
-OSMFILE = "example.osm"
+from util import defaultdict, logging_itr
+
+OSMFILE = "cincinnati_ohio.osm"
 street_type_re = re.compile(r'^(.*\s)(\S+\.?)$', re.IGNORECASE)
 
+class LimitedSet(set):
+    def __init__(self, *args, **kwargs):
+        n = None
+        if 'n' in kwargs:
+            n = kwargs['n']
+            del[kwargs['n']]
 
-expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
-            "Trail", "Parkway", "Commons"]
+        super(LimitedSet, self).__init__(*args, **kwargs)
+        self.n = n
+
+    def add(self, x):
+        if self.n is None or len(self) < self.n:
+            super(LimitedSet, self).add(x)
+
+
+# expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road",
+#             "Trail", "Parkway", "Commons"]
+expected = []
 
 # UPDATE THIS VARIABLE
 mapping = { "St": "Street",
@@ -36,24 +52,32 @@ def split_street(street_name):
         return street_name, None
 
 
-def audit_street_type(street_types, street_name):
+def get_street_type(street_name):
     _, street_type = split_street(street_name)
     if street_type is not None and street_type not in expected:
-        street_types[street_type].add(street_name)
+        return street_type
+    else:
+        return None
 
-def is_street_name(elem):
-    return (elem.attrib['k'] == "addr:street")
+def get_street_name(elem):
+    if elem.attrib.get('k') == 'addr:street':
+        return elem.attrib['v']
+    else:
+        return None
 
 
-def audit(osmfile):
-    osm_file = open(osmfile, "r")
-    street_types = defaultdict(set)
-    for event, elem in ET.iterparse(osm_file, events=("start",)):
-
+def get_unexpected_street_types(osmfile):
+    street_types = defaultdict(lambda: LimitedSet(n=5))
+    for _, elem in logging_itr(ET.iterparse(osmfile)):
         if elem.tag == "node" or elem.tag == "way":
             for tag in elem.iter("tag"):
-                if is_street_name(tag):
-                    audit_street_type(street_types, tag.attrib['v'])
+                name = get_street_name(tag)
+                if name is not None:
+                    street_type = get_street_type(name)
+                    if street_type is not None:
+                        street_types[street_type].add(name)
+        if elem.tag != 'tag':
+            elem.clear()
 
     return street_types
 
@@ -70,7 +94,11 @@ def update_name(name, mapping):
 def test():
     st_types = audit(OSMFILE)
     assert len(st_types) == 3
-    pprint.pprint(dict(st_types))
+    pprint.pprint(st_types)
+
+if __name__ == '__main__':
+    st_types = get_unexpected_street_types(OSMFILE)
+    pprint.pprint(st_types)
 
     for st_type, ways in st_types.iteritems():
         for name in ways:
@@ -80,7 +108,3 @@ def test():
                 assert better_name == "West Lexington Street"
             if name == "Baldwin Rd.":
                 assert better_name == "Baldwin Road"
-
-
-if __name__ == '__main__':
-    test()
--- python/util.py
+++ python/util.py
@@ -1,6 +1,11 @@
+import collections
+
 def logging_itr(itr, step=500000):
     for i, x in enumerate(itr, 1):
         if i % step == 0:
             print 'Finished {} items'.format(i)
         yield x
 
+
+class defaultdict(collections.defaultdict):
+    __repr__ = dict.__repr__
