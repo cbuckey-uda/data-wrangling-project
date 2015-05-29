9a20d2b0b7c919db531b9cf6dee7f02ad8cdb629
cbuckey <caroline@udacity.com>
2015-05-29 10:34:54 -0700

    Fix bug in count_tags

--- python/count_tags.py
+++ python/count_tags.py
@@ -18,7 +18,7 @@ def logging_itr(itr, step=100000):
     for i, x in enumerate(itr, 1):
         if i % step == 0:
             print 'Finished {} items'.format(i)
-            yield x
+        yield x
 
 def count_tags(filename):
     counts = defaultdict(int)
