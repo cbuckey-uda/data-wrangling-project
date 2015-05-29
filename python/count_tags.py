1d4aea01b1b3f3137d37af9653a9d805eef11f64
cbuckey <caroline@udacity.com>
2015-05-28 21:27:15 -0700

    Add code from L6 exercises

---
+++ python/count_tags.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Your task is to use the iterative parsing to process the map file and
+find out not only what tags are there, but also how many, to get the
+feeling on how much of which data you can expect to have in the map.
+Fill out the count_tags function. It should return a dictionary with the 
+tag name as the key and number of times this tag can be encountered in 
+the map as value.
+
+Note that your code will be tested with a different data file than the 'example.osm'
+"""
+import xml.etree.ElementTree as ET
+import pprint
+from collections import defaultdict
+
+def count_tags(filename):
+    counts = defaultdict(int)
+    for event, elem in ET.iterparse(filename):
+        counts[elem.tag] += 1
+    return dict(counts)
+
+
+def test():
+
+    tags = count_tags('example.osm')
+    pprint.pprint(tags)
+    assert tags == {'bounds': 1,
+                     'member': 3,
+                     'nd': 4,
+                     'node': 20,
+                     'osm': 1,
+                     'relation': 1,
+                     'tag': 7,
+                     'way': 1}
+
+    
+
+if __name__ == "__main__":
+    test()
---
+++ python/find_users.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+import pprint
+import re
+"""
+Your task is to explore the data a bit more.
+The first task is a fun one - find out how many unique users
+have contributed to the map in this particular area!
+
+The function process_map should return a set of unique user IDs ("uid")
+"""
+
+def get_user(element):
+    return element.attrib.get('uid')
+
+
+def process_map(filename):
+    users = set()
+    for _, element in ET.iterparse(filename):
+        user_id = get_user(element)
+        if user_id is not None:
+            users.add(user_id)
+
+    return users
+
+
+def test():
+
+    users = process_map('example.osm')
+    pprint.pprint(users)
+    assert len(users) == 6
+
+
+
+if __name__ == "__main__":
+    test()
