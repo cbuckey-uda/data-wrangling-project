e7e2ae59d1d37193d88eb793f07806076d1882f4
cbuckey <caroline@udacity.com>
2015-05-28 22:05:49 -0700

    Update and run 'count_tags.py'

--- python/count_tags.py
+++ python/count_tags.py
@@ -4,8 +4,8 @@
 Your task is to use the iterative parsing to process the map file and
 find out not only what tags are there, but also how many, to get the
 feeling on how much of which data you can expect to have in the map.
-Fill out the count_tags function. It should return a dictionary with the 
-tag name as the key and number of times this tag can be encountered in 
+Fill out the count_tags function. It should return a dictionary with the
+tag name as the key and number of times this tag can be encountered in
 the map as value.
 
 Note that your code will be tested with a different data file than the 'example.osm'
@@ -14,27 +14,19 @@ import xml.etree.ElementTree as ET
 import pprint
 from collections import defaultdict
 
+def logging_itr(itr, step=100000):
+    for i, x in enumerate(itr, 1):
+        if i % step == 0:
+            print 'Finished {} items'.format(i)
+            yield x
+
 def count_tags(filename):
     counts = defaultdict(int)
-    for event, elem in ET.iterparse(filename):
+    for event, elem in logging_itr(ET.iterparse(filename)):
         counts[elem.tag] += 1
+        elem.clear()
     return dict(counts)
 
-
-def test():
-
-    tags = count_tags('example.osm')
-    pprint.pprint(tags)
-    assert tags == {'bounds': 1,
-                     'member': 3,
-                     'nd': 4,
-                     'node': 20,
-                     'osm': 1,
-                     'relation': 1,
-                     'tag': 7,
-                     'way': 1}
-
-    
-
 if __name__ == "__main__":
-    test()
+    tags = count_tags('cincinnati_ohio.osm')
+    pprint.pprint(tags)
